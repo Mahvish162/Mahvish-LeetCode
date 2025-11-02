88bd64b6ec8137cfee504b01edc3526f61dceb00
Mahvish~Fatima <139191541+Mahvish162@users.noreply.github.com>
2025-11-02 16:29:27 +0530

    Time: 3 ms (34.78%), Space: 60.5 MB (40.48%) - LeetHub

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+  
+    let count=0;
+    let maxCount=0;
+
+    for(let i=0;i<=nums.length-1;i++){
+        
+        if(nums[i]==1){
+            count++;
+        }
+        else{
+          count=0;
+        }
+            maxCount=Math.max(count,maxCount);
+    }
+    
+    return maxCount;
+};
