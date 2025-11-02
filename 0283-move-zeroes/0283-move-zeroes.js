d0409138ca3a84a3ad9a9c5905c026ccd8978092
Mahvish~Fatima <139191541+Mahvish162@users.noreply.github.com>
2025-11-02 19:01:24 +0530

    Time: 3 ms (38.05%), Space: 60.5 MB (32.51%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -7,21 +7,45 @@ var moveZeroes = function(nums) {
 
     //brute force
     //store the non zero elements inside temp arr then shift temp arr elements into original array
-    let temp=new Array(n).fill(0)
+    // let temp=new Array(n).fill(0)
 
-    let index=0;
+    // let index=0;
+    // for(let i=0;i<n;i++){
+    //     if(nums[i]!=0){
+    //         temp[index]=nums[i]
+    //         index++;
+    //     }
+    // }
+    // // non zero elements shifted to temp array
+
+    // for(let i=0;i<n;i++){
+    //     nums[i]=temp[i];
+    // }
+    // temp array shifted to original array
+
+    // Time Complexity: O(N), we can move all zeroes to end in linear time.
+    // Space Complexity: O(N), additional space used for temporary array.
+
+
+    // OPTIMAL APPROACH
+
+    // find that point where first zero'th el starts 
+    let j=-1;
     for(let i=0;i<n;i++){
-        if(nums[i]!=0){
-            temp[index]=nums[i]
-            index++;
+        if(nums[i]==0){
+            j=i;
+            break;
         }
     }
-    // non zero elements shifted to temp array
+    if(j==-1)return
 
-    for(let i=0;i<n;i++){
-        nums[i]=temp[i];
+    for(let i=j+1;i<n;i++){
+        if(nums[i]!=0){
+            [nums[j],nums[i]]=[nums[i],nums[j]];
+            j++;
+        }
     }
-    // temp array shifted to original array
+    
 
 
 };
